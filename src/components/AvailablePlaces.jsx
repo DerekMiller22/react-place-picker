ce0c86e5c753fc736a86f7b27d321ca013abf27d
dmillions <derek.m360@gmail.com>
2024-08-09 14:47:32 -0400

    places call made to back end and imgs are displayed

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setPlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={places}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
