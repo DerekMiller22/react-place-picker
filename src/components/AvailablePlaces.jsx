fbc493597685ece14162ad2a4c0cc2d5aa276146
dmillions <derek.m360@gmail.com>
2024-08-09 14:50:29 -0400

    changed background call to use async/await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -5,13 +5,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [places, setPlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setPlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setPlaces(resData.places);
+    }
+    fetchPlaces();
   }, []);
 
   return (
